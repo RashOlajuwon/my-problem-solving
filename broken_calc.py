9fb865d16ad0ac711e52d1d87affaef046b45b99
Olajuwon Rasheed <75161912+RashOlajuwon@users.noreply.github.com>
2022-03-23 23:58:09 +0100

    Update broken_calc.py

--- broken_calc.py
+++ broken_calc.py
@@ -1,5 +1,9 @@
 """
+There is a broken calculator that has the integer startValue on its display initially. In one operation, you can:
 
+multiply the number on display by 2, or
+subtract 1 from the number on display.
+Given two integers startValue and target, return the minimum number of operations needed to display target on the calculator.
 """
 def brokenCalc(self, startValue: int, target: int) -> int:
         # Once target <= startValue, we only do subtract 1 to fit the target
