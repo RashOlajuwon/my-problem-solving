02c259a1ccc95055b66f2b13b3feed1b1590e87c
Olajuwon Rasheed <75161912+RashOlajuwon@users.noreply.github.com>
2022-03-25 16:16:52 +0100

    Update boats_to_save_people.py

--- boats_to_save_people.py
+++ boats_to_save_people.py
@@ -4,15 +4,28 @@ You are given an array people where people[i] is the weight of the ith person, a
 Return the minimum number of boats to carry every given person.
 """
 def numRescueBoats(self, people: List[int], limit: int) -> int:
-        people = sorted(people)
-        lo = 0
+#         people = sorted(people)
+#         lo = 0
+#         hi = len(people)-1
+#         boats = 0
+#         while lo <= hi:
+#             if people[lo] + people[hi] <= limit:
+#                 lo += 1
+#                 hi -= 1
+#             else:
+#                 hi -= 1
+#             boats += 1
+#         return boats 
         hi = len(people)-1
-        boats = 0
-        while lo <= hi:
-            if people[lo] + people[hi] <= limit:
-                lo += 1
-                hi -= 1
+        lo = 0
+        ppl = sorted(people)
+        # print(ppl)
+        cnt = 0
+        while lo<=hi:
+            if ppl[lo]+ppl[hi]<=limit:
+                cnt+=1
+                lo+=1
             else:
-                hi -= 1
-            boats += 1
-        return boats 
+                cnt+=1
+            hi-=1
+        return cnt
