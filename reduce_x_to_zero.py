d64d4125edcc6be823f67c99a014ce6184f1ffdf
Olajuwon Rasheed <75161912+RashOlajuwon@users.noreply.github.com>
2022-06-14 12:19:17 +0100

    Create reduce_x_to_zero.py

--- reduce_x_to_zero.py
+++ reduce_x_to_zero.py
@@ -3,21 +3,23 @@ You are given an integer array nums and an integer x. In one operation, you can 
 Return the minimum number of operations to reduce x to exactly 0 if it is possible, otherwise, return -1.
 """
 
-def minOperations(self, nums: List[int], x: int) -> int:
-        steps = [0]
-        dp = {}
-        s = ""
-        for i in nums:
-            s += str(i)
-        def reduce(x,s):
-            if x==0:
-                dp[(x,s)]=1
-                return 1
-            if x<0:
-                return 100000
-            if (x,s) in dp:
-                return dp[(x,s)]
-            dp[(x,s)] = 1+min(reduce(x-int(s[0]),s[1:]),reduce(x-int(s[-1]),s[:-1]))
-            return dp[(x,s)]
-        a = reduce(x,s)
-        return a if a<1000 else -1
+# def minOperations(self, nums: List[int], x: int) -> int:
+#         steps = [0]
+#         dp = {}
+#         s = ""
+#         for i in nums:
+#             s += str(i)
+#         def reduce(x,s):
+#             if x==0:
+#                 dp[(x,s)]=1
+#                 return 1
+#             if x<0:
+#                 return 100000
+#             if (x,s) in dp:
+#                 return dp[(x,s)]
+#             dp[(x,s)] = 1+min(reduce(x-int(s[0]),s[1:]),reduce(x-int(s[-1]),s[:-1]))
+#             return dp[(x,s)]
+#         a = reduce(x,s)
+#         return a if a<1000 else -1
+
+# needs good solution
